5dc925f208f07a373bb7e0c796ddb1cd971b8765
OneWittyGrassBlade <jillphys@hotmail.com>
2019-11-12 19:52:53 +0000

    Update PixiJS to version 5

--- src/const/aliases.js
+++ src/const/aliases.js
@@ -1,13 +1,12 @@
-import 'pixi.js';
+import * as PIXI from 'pixi.js';
 
 /* eslint-disable no-undef */
-export const { autoDetectRenderer } = PIXI;
 export const { Application } = PIXI;
 export const { Graphics } = PIXI;
 export const { Texture } = PIXI;
 export const { Container } = PIXI;
-export const { ParticleContainer } = PIXI.particles;
-export const { loader } = PIXI;
-export const { resources } = PIXI.loader;
+export const { ParticleContainer } = PIXI;
+export const loader = PIXI.Loader.shared;
+export const { resources } = loader;
 export const { Sprite } = PIXI;
 /* eslint-enable no-undef */
